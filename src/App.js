f8f44580eb9f6f23650e930647b01e1da181fa04
Ville Andersson <winstoni@windowslive.com>
2021-11-18 01:53:19 +0200

    todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [kuvaus, setKuvaus] = useState("");
+  const [pvm, setPvm] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setKuvaus(event.target.value);
+  };
+  const inputChanged2 = (event) => {
+    setPvm(event.target.value);
+  };
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, [kuvaus, pvm]]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">TODOLIST</h1>
+      <div className="App-input">
+        <form onSubmit={addTodo}>
+          Description:
+          <input type="text" value={kuvaus} onChange={inputChanged} />
+          Date:
+          <input type="text" value={pvm} onChange={inputChanged2} />
+          <input type="submit" value="Add" />
+        </form>
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+          </tr>
+          {todos.map((todo, index) => (
+            <tr key={index}>
+              <td>{todo[0]}</td>
+              <td>{todo[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
